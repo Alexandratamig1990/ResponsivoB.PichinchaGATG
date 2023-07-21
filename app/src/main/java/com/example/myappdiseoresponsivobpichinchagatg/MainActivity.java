9ab40f03c0b859fdacfcdedc84056da14a59b8a4
Glanda Tamami <135934776+Alexandratamig1990@users.noreply.github.com>
2023-07-21 01:40:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myappdiseoresponsivobpichinchagatg/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myappdiseoresponsivobpichinchagatg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
